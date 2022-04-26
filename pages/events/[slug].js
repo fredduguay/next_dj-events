ac606e620df39116b5b5f62419bc74f1f22412eb
Fred Duguay <fredduguay@users.noreply.github.com>
2022-04-26 14:49:16 -0400

    add singleEvent page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,18 +1,57 @@
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
 import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
 import Layout from '@/components/Layout';
 import { API_URL } from '@/config/index';
+import styles from '@/styles/Event.module.css';
 // import { useRouter } from 'next/router';
 
 export default function EventPage({ evt }) {
   // const router = useRouter();
-
   // console.log(router);
+
+  const deleteEvent = (e) => {
+    console.log('delete');
+  };
 
   return (
     <Layout>
-      <h1>{evt.name}</h1>
-      {/* <h3>{router.query.slug}</h3>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href='#' className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href='/events'>
+          <a className={styles.back}>{'<'} Go back</a>
+        </Link>
+        {/* #ROUTER Example */}
+        {/* <h3>{router.query.slug}</h3>
       <button onClick={() => router.push('/')}>Send</button> */}
+      </div>
     </Layout>
   );
 }
